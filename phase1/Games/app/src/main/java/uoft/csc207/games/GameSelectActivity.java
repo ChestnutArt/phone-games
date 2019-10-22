7233fbb2eb7455a6e9274537b2b3c4c51da0bd3b
chuwill4 <w.chu@mail.utoronto.ca>
2019-10-21 23:06:47 -0400

    Changed the onCreate method of LoginActivity to add a listener to the login button. Added a validate method in LoginActivity which is called when the login button is clicked. Also created GameSelectActivity which the app transitions to if login is successful.

---
+++ phase1/Games/app/src/main/java/uoft/csc207/games/GameSelectActivity.java
@@ -0,0 +1,14 @@
+package uoft.csc207.games;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_select);
+    }
+}
--- phase1/Games/app/src/main/java/uoft/csc207/games/LoginActivity.java
+++ phase1/Games/app/src/main/java/uoft/csc207/games/LoginActivity.java
@@ -2,13 +2,37 @@ package uoft.csc207.games;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.etName);
+        password = (EditText) findViewById(R.id.etPassword);
+        login = (Button) findViewById(R.id.btnLogin);
+
+        login.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View view){
+               validate(username.getText().toString(), password.getText().toString());
+           }
+        });
+    }
+
+    private void validate(String userName, String passWord){
+        if (userName.equals("1234") && passWord.equals("5678")){
+            Intent myIntent = new Intent(LoginActivity.this, GameSelectActivity.class);
+            startActivity(myIntent);
+        }
     }
 }
