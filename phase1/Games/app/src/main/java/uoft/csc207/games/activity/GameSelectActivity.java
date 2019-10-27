9cf7b9f32b2033ea301c92bbb3886384483926ef
chuwill4 <w.chu@mail.utoronto.ca>
2019-10-26 22:25:43 -0400

    Created and started a bit on Game class. Also created empty setOnClickListeners in GameSelectActivity for the game buttons, you should write code to have them move to your game activity.

--- phase1/Games/app/src/main/java/uoft/csc207/games/activity/GameSelectActivity.java
+++ phase1/Games/app/src/main/java/uoft/csc207/games/activity/GameSelectActivity.java
@@ -16,12 +16,22 @@ public class GameSelectActivity extends AppCompatActivity {
     private TextView welcome;
     private Button logout;
     private PlayerProfile currentProfile;
+
+    private Button scrollerSelect;
+    private Button cardSelect;
+    private Button rpgSelect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_select);
         welcome = findViewById(R.id.tvWelcome);
         logout = findViewById(R.id.btnLogout);
+
+        scrollerSelect = findViewById(R.id.btnScroller);
+        cardSelect = findViewById(R.id.btnCard);
+        rpgSelect = findViewById(R.id.btnRPG);
+
         Intent intent = getIntent();
         currentProfile = (PlayerProfile)intent.getSerializableExtra(ProfileManager.CURRENT_PLAYER);
         welcome.setText("Welcome back " + currentProfile.getId() + "!");
@@ -34,6 +44,26 @@ public class GameSelectActivity extends AppCompatActivity {
             }
         });
 
+        scrollerSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jump to the scroller game activity here
+            }
+        });
+
+        cardSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jump to the card game activity here
+            }
+        });
+
+        rpgSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jump to the rpg game activity here
+            }
+        });
     }
 
     private void moveToLoginActivity(){
---
+++ phase1/Games/app/src/main/java/uoft/csc207/games/model/Game.java
@@ -0,0 +1,13 @@
+package uoft.csc207.games.model;
+
+import android.content.Intent;
+
+import java.util.ArrayList;
+
+public class Game {
+    private int gameScore;
+    private int gameCurrency;
+
+    private static String[] characters = {"male", "female"};
+
+}
