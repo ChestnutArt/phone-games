76c42fdf00f12c5d4608dd17b64b9bf7af4e6696
Anindya Auveek <anindya.auveek@mail.utoronto.ca>
2019-11-28 17:38:37 -0500

    ScoreBoard Classes

--- phase1/Games/app/src/main/java/uoft/csc207/games/model/Ranker.java
+++ phase1/Games/app/src/main/java/uoft/csc207/games/model/Ranker.java
@@ -1,80 +1,55 @@
 package uoft.csc207.games.model;
 
-import android.provider.ContactsContract;
 
 import java.util.ArrayList;
-import java.util.Collection;
-
-import uoft.csc207.games.controller.ProfileManager;
+import uoft.csc207.games.controller.Score;
 
 public class Ranker {
-    private ArrayList profiles;
-    public Ranker(ArrayList profiles){
-        this.profiles = profiles;
+    public ArrayList<Score> scores;
+    public Ranker(ArrayList scores){
+        this.scores = scores;
     }
 
-    public ArrayList createListByScore(int l, int r){
+    public ArrayList<Score> createListByScore(int l, int r){
         if (l >= r){
-            return profiles;
+            return scores;
         }
         else{
-            PlayerProfile pivot = (PlayerProfile)profiles.get(r);
+            Score pivot = scores.get(r);
             int cnt = l;
-            for (int i = l; i <= profiles.size(); i++)
+            for (int i = l; i <= scores.size(); i++)
             {
-                PlayerProfile p = (PlayerProfile)profiles.get(i);
-                if (p.getScore() <= pivot.getScore())
+                Score p = scores.get(i);
+                if (p.getPoints() <= pivot.getPoints())
                 {
-                    swap(profiles.get(cnt), profiles.get(i));
+                    swap(scores.get(cnt), scores.get(i));
                     cnt++;
                 }
             }
             createListByScore(l, cnt - 2);
             createListByScore(cnt, r);
-            return profiles;
+            return scores;
         }
     }
-    public ArrayList createListByCurrency(int l, int r){
+    public ArrayList<Score> createListByCurrency(int l, int r){
         if (l >= r){
-            return profiles;
+            return scores;
         }
         else{
-            PlayerProfile pivot = (PlayerProfile)profiles.get(r);
+            Score pivot = scores.get(r);
             int cnt = l;
-            for (int i = l; i <= profiles.size(); i++)
+            for (int i = l; i <= scores.size(); i++)
             {
-                PlayerProfile p = (PlayerProfile)profiles.get(i);
-                if (p.getCurrency() <= pivot.getCurrency())
+                Score p = scores.get(i);
+                if (p.getMoney() <= pivot.getMoney())
                 {
-                    swap(profiles.get(cnt), profiles.get(i));
+                    swap(scores.get(cnt), scores.get(i));
                     cnt++;
                 }
             }
             createListByCurrency(l, cnt - 2);
             createListByCurrency(cnt, r);
-            return profiles;
-        }
-    }
-
-    public ArrayList createListByAchievements(int l, int r){
-        if (l >= r){
-            return profiles;
-        }
-        else{
-            PlayerProfile pivot = (PlayerProfile)profiles.get(r);
-            int cnt = l;
-            for (int i = l; i <= profiles.size(); i++)
-            {
-                PlayerProfile p = (PlayerProfile)profiles.get(i);
-                if (p.getNumAchievements() <= pivot.getNumAchievements())
-                {
-                    swap(profiles.get(cnt), profiles.get(i));
-                    cnt++;
-                }
-            }
-            createListByAchievements(l, cnt - 2);
-            createListByAchievements(cnt, r);
-            return profiles;
+            return scores;
         }
     }
 
@@ -83,6 +58,14 @@ public class Ranker {
         Object temp = a;
         a = b;
         b = temp;
+    }
+
+    public void addScore(Score p){
+       scores.add(p);
+    }
+
+    public int getSize(){
+        return scores.size();
     }
 
 }
